df27ba29e5625177eaeecbf1c89c91c3665b572c
CoasterFreakDE <mleaked@gmail.com>
2022-11-25 17:24:23 +0100

    ViteConfig changes

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/welcome.js',
+                'resources/js/dashboard.js',
             ],
             refresh: true,
         }),
